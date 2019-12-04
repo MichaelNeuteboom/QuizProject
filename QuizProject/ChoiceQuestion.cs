b6cc61e521b4b6fbdcdd0a56a209bf01344c3214
MichaelNeuteboom <michaelneuteboom1@gmail.com>
2019-12-04 14:00:24 +0100

    ChoiceQuestion and Question

---
+++ QuizProject/ChoiceQuestion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizProject
+{
+    public class ChoiceQuestion : Question
+    {
+        private List<String> choices;
+        public ChoiceQuestion()
+        {
+            choices = new List<String>();
+        }
+        public void addChoice(String choice, bool correct)
+        {
+            
+            choices.Add(choice); if (correct)
+            {
+        // Convert choices.size() to string
+        String choiceString = "" + choices.Count; setAnswer(choiceString);
+            }
+        }
+        public void display()
+        {
+            // Display the question text super.display();
+            // Display the answer choices
+            for (int i = 0; i < choices.Count; i++)
+            {
+                int choiceNumber = i + 1;
+                Console.WriteLine(choiceNumber + ": " + choices[i]);
+                
+            }
+        }
+    }
+}
---
+++ QuizProject/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace QuizProject
+{
+    public class Question
+    {
+        private string text;
+        private string answer;
+      public Question()
+        {
+            text = "";
+            answer = "";
+        }
+
+        public void setText(String questionText)
+        {
+            text = questionText;
+        }
+    
+        public void setAnswer(String correctResponse)
+        {
+            answer = correctResponse;
+        }
+    
+        public bool checkAnswer(String response)
+        {
+            
+            return response.Equals(answer);
+        }
+   
+        public void display()
+        {
+            Console.WriteLine(text);
+            
+        }
+    }
+}
